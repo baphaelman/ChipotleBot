26b0e56ef64cbacf75b9ef34b70f77dd70f62fd5
Raphael Bajet <baphael@berkeley.edu>
2024-10-16 02:17:51 -0700

    componetized game into playGame and printOutcome functions

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 import chess
 import random
 
-board = chess.Board()
-outcome = board.outcome()
+def playGame(board) -> chess.Outcome:
+    outcome = board.outcome()
 
-while not outcome:
-    print(board)
+    while not outcome:
+        print(board)
 
-    # my move
-    my_move = input("Enter your move: ")
-    board.push_san(my_move)
+        # my move
+        my_move = input("Enter your move: ")
+        board.push_san(my_move)
 
-    outcome = board.outcome() # resetting outcome
-    if outcome:
-        break
+        outcome = board.outcome() # resetting outcome
+        if outcome:
+            return outcome
 
-    # computer's move
-    random_move = random.choice(list(board.legal_moves))
-    board.push(random_move)
+        # computer's move
+        random_move = random.choice(list(board.legal_moves))
+        board.push(random_move)
 
-    outcome = board.outcome() # resetting outcome
+        outcome = board.outcome() # resetting outcome
+    return outcome
 
-outcome = "White" if outcome.winner == chess.WHITE else "Black"
+def printOutcome(outcome) -> None:
+    outcome = "White" if outcome.winner == chess.WHITE else "Black"
+    print(f"Game over. {outcome} wins!")
 
-print(f"Game over. {outcome} wins!")
+def main() -> None:
+    board = chess.Board()
+
+    outcome = playGame(board)
+    printOutcome(outcome)
+
+if __name__ == "__main__":
+    main()
