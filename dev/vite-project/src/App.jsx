5e74c43e8343fa0efb18a0bd235f4c042270b72c
Raphael Bajet <baphael@berkeley.edu>
2024-11-27 01:12:54 -0800

    inplemented input bar to type out moves

--- dev/vite-project/src/App.jsx
+++ dev/vite-project/src/App.jsx
@@ -1,18 +1,40 @@
 import './App.css'
+import { useState } from 'react'
 import ChessBoard from './components/ChessBoard'
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSubmit();
+    }
+  }
+
+  // this is where my code should interact with the backend in the chipotlebot code
+  const handleSubmit = () => {
+    console.log('Input value: ', inputValue);
+    setInputValue('');
+  }
 
   return (
     <div className="app">
       <div className="text">
         <h1>ChipotleBot</h1>
-        <h2>pay up,, sucker</h2>
+        <h3>pay up,, sucker</h3>
       </div>
       <div className="board">
         <ChessBoard />
       </div>
-      <input></input>
+      <input
+        value={inputValue}
+        onChange={handleInputChange}
+        onKeyUp={handleKeyPress}
+      />
     </div>
   )
 }
